c916d0ae34336e0aee208b94ec3ea7492ebc0ce7
Константин Белан <belankostya@yandex.ru>
2025-06-28 23:22:06 +0300

    HomeWorks

---
+++ ClassWork3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int absoluteValue = 0;
+            if (number > 0)
+            {
+                absoluteValue = number;
+            }
+            else
+            {
+                absoluteValue = -number;
+            }
+            Console.WriteLine("Absolute value: " + absoluteValue);
+        }
+    }
+}
